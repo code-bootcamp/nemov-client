21daf5f65af5dc1d98303f1efecca712754a45d8
lsenaa <lsenaa3838@gmail.com>
2022-12-26 18:54:12 +0900

    🎉 create develop project

---
+++ src/commons/styles/globalStyles.ts
@@ -0,0 +1,61 @@
+import { css } from "@emotion/react";
+
+export const globalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-size: 16px;
+    font-family: "SUIT-Regular";
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  @font-face {
+    font-family: "SUIT-Bold";
+    src: url("/fonts/SUIT-Bold.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-ExtraBold";
+    src: url("/fonts/SUIT-ExtraBold.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-ExtraLight";
+    src: url("/fonts/SUIT-ExtraLight.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-Heavy";
+    src: url("/fonts/SUIT-Heavy.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-Light";
+    src: url("/fonts/SUIT-Light.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-Medium";
+    src: url("/fonts/SUIT-Medium.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-Regular";
+    src: url("/fonts/SUIT-Regular.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-SemiBold";
+    src: url("/fonts/SUIT-SemiBold.ttf");
+  }
+
+  @font-face {
+    font-family: "SUIT-Thin";
+    src: url("/fonts/SUIT-Thin.ttf");
+  }
+`;
