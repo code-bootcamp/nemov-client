5fa8e239886a3cfdd8bb279a70509510c61f0927
lsenaa <lsenaa3838@gmail.com>
2022-12-27 19:11:21 +0900

    💄 Add restaurant mockup

---
+++ pages/restaurant/reviews/new/index.tsx
@@ -0,0 +1,5 @@
+import ReviewWrite from "../../../../src/components/units/restaurant-review/write/ReviewWrite.index";
+
+export default function ReviewWritePage() {
+  return <ReviewWrite />;
+}
---
+++ src/components/units/restaurant-review/list/ReviewList.index.tsx
@@ -0,0 +1,9 @@
+export default function ReviewList() {
+  return (
+    <>
+      <div style={{ width: "70%", height: "10rem", border: "1px solid blue" }}>
+        식당 후기 리스트
+      </div>
+    </>
+  );
+}
