504cbe62973915820163cfbebb9a3d9fd56abe21
namYeJi9q <nyj5850@naver.com>
2023-01-02 02:47:09 +0900

    💄 Update seller page #65

---
+++ src/components/units/product/register/category.ts
@@ -0,0 +1,62 @@
+export const categoryTitle = ["FOOD", "DRINK", "BEAUTY", "LIFE"];
+
+export const categoryContents = [
+  [
+    "품명 및 모델명",
+    "인증/허가 사항",
+    "제조국(원산지)",
+    "제조자(수입자)",
+    "소비자상담 관련 전화번호",
+  ],
+  [
+    "품명 및 모델명",
+    "인증/허가 사항",
+    "제조국(원산지)",
+    "제조자(수입자)",
+    "소비자상담 관련 전화번호",
+  ],
+  [
+    "품명 및 모델명",
+    "용량 또는 중량",
+    "제조국",
+    "제품 주요 사양",
+    "화장품법에 따라 기재해야하는 모든 성분",
+    "사용기한 또는 개봉 후 사용기간",
+    "KC 인증 필 유무",
+    "사용방법",
+    "제조자(수입자)",
+    "품질보증기준",
+    "A/S 책임자와 전화번호",
+  ],
+  [
+    "품명 및 모델명",
+    "인증/허가 사항",
+    "제조국(원산지)",
+    "제조자(수입자)",
+    "소비자상담 관련 전화번호",
+  ],
+];
+
+export const selectCategory = [
+  {
+    value: "default",
+    disabled: true,
+    label: "선택하세요",
+  },
+  {
+    value: "FOOD",
+    label: "FOOD",
+  },
+  {
+    value: "DRINK",
+    label: "DRINK",
+  },
+  {
+    value: "BEAUTY",
+    label: "BEAUTY",
+  },
+  {
+    value: "LIFE",
+    label: "LIFE",
+  },
+];
