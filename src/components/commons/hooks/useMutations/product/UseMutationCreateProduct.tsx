2c277db8223c0029c4696d522560b5bcd9889b74
mibu119 <horang296@gmail.com>
2023-01-17 21:49:24 +0900

    🔨 add gql

--- src/components/commons/hooks/useMutations/product/UseMutationCreateProduct.tsx
+++ src/components/commons/hooks/useMutations/product/UseMutationCreateProduct.tsx
@@ -3,7 +3,11 @@ import {
   IMutation,
   IMutationCreateProductArgs,
 } from "../../../../../commons/types/generated/types";
-import { FETCH_PRODUCTS_COUNT_BY_SELLER } from "../../useQueries/product/UseQueryFetchProductsBySeller";
+import {
+  FETCH_PRODUCTS_BY_SELLER,
+  FETCH_PRODUCTS_COUNT_BY_SELLER,
+} from "../../useQueries/product/UseQueryFetchProductsBySeller";
+import { FETCH_PRODUCTS_COUNT } from "../../useQueries/product/UseQueryFetchProductsCount";
 
 export const CREATE_PRODUCT = gql`
   mutation createProduct(
@@ -22,7 +26,13 @@ export const CREATE_PRODUCT = gql`
 export const UseMutationCreateProduct = () => {
   const mutation = useMutation<Pick<IMutation, "createProduct">, IMutationCreateProductArgs>(
     CREATE_PRODUCT,
-    { refetchQueries: [{ query: FETCH_PRODUCTS_COUNT_BY_SELLER }] }
+    {
+      refetchQueries: [
+        { query: FETCH_PRODUCTS_COUNT_BY_SELLER },
+        { query: FETCH_PRODUCTS_BY_SELLER, variables: { page: 1 } },
+        { query: FETCH_PRODUCTS_COUNT },
+      ],
+    }
   );
   return mutation;
 };
